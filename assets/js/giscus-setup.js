cc644a6127b42bc7973e89e04ec48546f0963460
xueyan99 <112151105+xueyan99@users.noreply.github.com>
2026-04-08 12:10:53 +0000

    Deploying to gh-pages from @ xueyan99/academic2026@bbe3a7ff77b51f9aae392dd18d75abf7c1125d71 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "xueyan22/academic2026",
+    "data-repo": "xueyan99/academic2026",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
